0c80d220a6dad0fbfb6d955b86d0da773e484a3b
Sergio Alvarez-Napagao <sergio.alvarez@bsc.es>
2021-05-09 15:53:37 +0000

    Add monitoring stage

---
+++ monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+let "a = 0"
+while [[ "$(squeue | grep $JOB_PID)" != "" ]]
+do
+  if [[ $((a % 5)) == 0 ]]; then
+    squeue -l
+    squeue -l --start
+  fi
+
+  cat $JOB_WORKDIR/output/*_out.txt | tail -n 15
+  echo "Still running..."
+  sleep 600
+  let "a = a + 1"
+done
