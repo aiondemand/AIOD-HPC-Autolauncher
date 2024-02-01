5773aaa298826a08430c8b280a615badb11e8599
Marta Gonzalez Mallo <marta.gonzalez@bsc.es>
2024-02-01 17:53:14 +0100

    noninteractive mode & no StrictHostKeyChecking

--- utilities/cesga_vpn.sh
+++ utilities/cesga_vpn.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+./snx -d 
+
 expect -c "
-	spawn sudo ./snx -s secure.cesga.es -u $1
+	spawn ./snx -s secure.cesga.es -u $1
 	expect \"Please enter your password:\"
 	send \"$2\r\"
 	expect \"Do you accept? \[y\]es/ \[N\]o:\"
