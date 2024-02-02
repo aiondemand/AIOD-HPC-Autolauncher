350ffcb30f95659b4c2c82295f909b7db0c0ad6c
Marta Gonzalez Mallo <marta.gonzalez@bsc.es>
2024-02-02 09:53:20 +0100

    Refractor cesga autolauncher

---
+++ utilities/monitor_cesga.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+let "a = 0"
+while [[ "$(squeue | grep $JOB_PID)" != "" ]]
+do
+  if [[ $((a % 5)) == 0 ]]; then
+    squeue -l
+    squeue -l --start
+  fi
+
+  cat $OUT_FILE | tail -n 15
+  echo "Still running..."
+  sleep 60
+  let "a = a + 1"
+done
