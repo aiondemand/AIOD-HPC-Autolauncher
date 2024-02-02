017cd1ef6a82ce97afe8813b65523ea8d09f639c
Marta Gonzalez Mallo <marta.gonzalez@bsc.es>
2024-02-02 16:21:04 +0100

    use local cesga image

--- utilities/cesga_vpn.sh
+++ utilities/cesga_vpn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./snx -d 
+snx -d 
 
 expect -c "
-	spawn ./snx -s secure.cesga.es -u $1
+	spawn snx -s secure.cesga.es -u $1
 	expect \"Please enter your password:\"
 	send \"$2\r\"
 	expect \"Do you accept? \[y\]es/ \[N\]o:\"
