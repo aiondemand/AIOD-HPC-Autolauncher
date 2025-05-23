64e507a975433200673b258549e857885723ebd8
Kike <kikelopez034@gmail.com>
2025-05-23 12:34:56 +0200

    fix leonardo monitor by adding user to squeue

--- utilities/monitor.sh
+++ utilities/monitor.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+USER=$1
+SQUEUE_CMD="squeue"
+if [ ! -z "$USER" ]; then
+    SQUEUE_CMD="squeue -u $USER"
+fi
+
 let "a = 0"
+
 while [[ "$(squeue | grep $JOB_PID)" != "" ]]
 do
   if [[ $((a % 5)) == 0 ]]; then
-    squeue -l
-    squeue -l --start
+    $SQUEUE_CMD -l
+    $SQUEUE_CMD -l --start
   fi
 
   cat ${OUT_FILE} | tail -n 15
